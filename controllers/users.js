f6d2db19b439389362a2fc2ebbf1d1fb5978e8f6
Oleksandr Pomalin <nilamopr@gmail.com>
2021-06-22 22:37:19 +0300

    adds passport

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,26 @@
 const Users = require('../repositories/users')
-const = {HttpCode} = require('../helpers/constants')
+const { HttpCode } = require('../helpers/constants')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const SECRET_KEY = process.env.SECRET_KEY
 
 const signup = async (req, res, next) => {
   try {
     const user = await Users.findByEmail(req.body.email)
     if (user) {
-          return res.status(HttpCode.CONFLICT).json({ status: 'error', code: HttpCode.CONFLICT, message: 'Email in use' })
-
+      return res.status(HttpCode.CONFLICT).json({
+        status: 'Conflict',
+        code: HttpCode.CONFLICT,
+        message: 'Email in use',
+      })
     }
-    return res.json({ status: 'success', code: 200, data: { contacts } })
+    const { id, name, email, subscription } = await Users.create(req.body)
+
+    return res.status(HttpCode.CREATED).json({
+      status: 'success',
+      code: HttpCode.CREATED,
+      data: { id, name, email, subscription },
+    })
   } catch (e) {
     next(e)
   }
@@ -16,8 +28,24 @@ const signup = async (req, res, next) => {
 
 const login = async (req, res, next) => {
   try {
-    const contacts = await Users.listContacts()
-    return res.json({ status: 'success', code: 200, data: { contacts } })
+    const user = await Users.findByEmail(req.body.email)
+    const isValidPassword = await user?.isValidPassword(req.body.password)
+    if (!user || !isValidPassword) {
+      return res.status(HttpCode.UNAUTHORIZED).json({
+        status: 'error',
+        code: HttpCode.UNAUTHORIZED,
+        message: 'Email or password is wrong',
+      })
+    }
+    const id = user.id
+    const payload = { id, test: 'Larisa the best' }
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '2h' })
+    await Users.updateToken(id, token)
+    return res.status(HttpCode.OK).json({
+      status: 'success',
+      code: HttpCode.OK,
+      data: { token },
+    })
   } catch (e) {
     next(e)
   }
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router()
-
 const controller = require('../../../controllers/users')
 
-router.post('/users/signup', controller.signup)
-router.post('/users/login', controller.login)
-router.post('/users/logout', controller.logout)
+router.post('/signup', controller.signup)
+router.post('/login', controller.login)
+router.post('/logout', controller.logout)
 
 module.exports = router
