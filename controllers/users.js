d335fb2f2acd8804fd4c796dea62ea03f64d2dc7
Oleksandr Pomalin <nilamopr@gmail.com>
2021-06-24 00:30:25 +0300

    adds controller logout

--- controllers/users.js
+++ controllers/users.js
@@ -53,8 +53,9 @@ const login = async (req, res, next) => {
 
 const logout = async (req, res, next) => {
   try {
-    const contacts = await Users.listContacts()
-    return res.json({ status: 'success', code: 200, data: { contacts } })
+    const id = req.user.id
+    await Users.updateToken(id, null)
+    return res.status(HttpCode.NO_CONTENT).json({})
   } catch (e) {
     next(e)
   }
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../../../controllers/users')
+const guard = require('../../../helpers/guard')
 
 router.post('/signup', controller.signup)
 router.post('/login', controller.login)
-router.post('/logout', controller.logout)
+router.post('/logout', guard, controller.logout)
 
 module.exports = router
